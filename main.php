f3e565c833ba3c41f493a09c479da179f2b2b528
Isaenko Alexei <sh-v@ya.ru>
2019-01-08 13:25:55 +0300

    add ver hardcode function

--- main.php
+++ main.php
@@ -1,6 +1,12 @@
 <?php
 namespace myspace;
 
+require 'create-tables.php';
+
+function get_version() {
+	return '1.0.0';
+}
+
 function get_default_columns() {
 	$prefix = '';
 	$data   = array(
@@ -20,5 +26,4 @@ function get_default_columns() {
 
 add_filter( __NAMESPACE__ . '\create_tables', __NAMESPACE__ . '\get_default_columns', 10, 1 );
 
-require 'includes/create-tables.php';
 register_activation_hook( __FILE__, __NAMESPACE__ . '\create_tables' );
